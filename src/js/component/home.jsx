7d90852d7d2048ca905c6152c8294d90bb6c3b73
Yurbanis <yurbanisbriceno@gmail.com>
2023-03-03 02:22:35 +0000

    Se realiza modificacion del archivo home para realizar counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,47 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import '../../styles/index.css';
+
 
 //create your first component
-const Home = () => {
+function Home(props) {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	  <div className="mySuperStyles">
+		<div className="reloj">
+		<div className="signo">{props.signo}</div>
+		  <div className="cinco">{props.number1}</div>
+		  <div className="cuatro">{props.number2}</div>
+		  <div className="tres">{props.number3}</div>
+		  <div className="dos">{props.number4}</div>
+		  <div className="uno">{props.number5}</div>
 		</div>
+	  </div>
 	);
-};
+  }
+
+  Home.propTypes = {
+	signo: PropTypes.string,
+	number1: PropTypes.string,
+	number2: PropTypes.string,
+	number3: PropTypes.string,
+	number4: PropTypes.string,
+	number5: PropTypes.string,
+  };
+
 
 export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,18 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+setInterval(function () {
+  let segundos = Math.floor(Math.random() * 10);
+  let minutos = Math.floor(Math.random() * 10);
+
+  ReactDOM.render(
+    <Home 
+          signo =<i class="fa-light fa fa-clock"></i>
+           number1="0"
+          number2="0" 
+          number3="0"
+          number4={minutos}
+          number5={segundos} />, document.querySelector("#app"));
+}, 1000);
+
