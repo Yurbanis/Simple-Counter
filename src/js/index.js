2416f8c4cdfd731c05c2f17189a9daa09ed0d25f
Yurbanis <yurbanisbriceno@gmail.com>
2023-03-27 03:45:25 +0000

    se modifica counter

--- src/js/index.js
+++ src/js/index.js
@@ -9,23 +9,30 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-let segundos = Math.floor(Math.random() * 3);
+let segundos = 0;
+let minutos = 0;
+let horas = 0;
 
 setInterval(function () {
-  if(segundos <= 4) {
-    segundos++
-  } else {
-    segundos = Math.floor(Math.random() * 4);
-  }
-   
-     ReactDOM.render(
-       <Home 
-             signo =<i className="fa-light fa fa-clock"></i>
-              number1="0"
-             number2="0" 
-             number3="0"
-             number4="0"
-             number5={segundos} />, document.querySelector("#app"));
-  
-}, 1000);
+  segundos++;
 
+  if (segundos === 60) {
+    segundos = 0;
+    minutos++;
+  } else if (minutos === 60) {
+    minutos = 0;
+    horas++;
+  }
+
+  ReactDOM.render(
+    <Home
+      signo=<i className="fa-light fa fa-clock"></i>
+      number1="0"
+      number2="0"
+      number3={horas}
+      number4={minutos}
+      number5={segundos}
+    />,
+    document.querySelector("#app")
+  );
+}, 1000);
